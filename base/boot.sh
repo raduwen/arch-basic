6a43c076f84714ce4b0ec5b619eec98961009092
raduwen <raduwen@gmail.com>
2021-12-28 14:26:22 +0900

    fix boot

--- base/boot.sh
+++ base/boot.sh
@@ -2,7 +2,17 @@
 
 ohai "Boot"
 
-pacman -S --noconfirm --needed grub efibootmgr
+bootctl install
+cat >/boot/loader/loader.conf <<EOS
+default arch
+timeout 3
+editor no
+EOS
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+if [ ! -f /boot/loader/entries/arch.conf ]; then
+  echo "title Arch Linux" >/boot/loader/entries/arch.conf
+  echo "linux /vmlinuz-linux" >>/boot/loader/entries/arch.conf
+  echo "initrd /intel-ucode.img" >>/boot/loader/entries/arch.conf
+  echo "initrd /initramfs-linux.img" >>/boot/loader/entries/arch.conf
+  echo "options root=$(blkid -o export /dev/sda1)" rw >>/boot/loader/entires/arch.conf
+fi
