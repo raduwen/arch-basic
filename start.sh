d878c7dfddef7e7bcac1912588545c58fb0663b6
raduwen <raduwen@gmail.com>
2021-12-28 10:21:41 +0900

    w

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(
-  cd $(dirname $0)
+  cd "$(dirname "$0")"
   pwd
 )"
 
-source "${SCRIPT_DIR}/func/print.sh"
+source "$SCRIPT_DIR/func/print.sh"
 
 source "$SCRIPT_DIR/base/swap.sh"
 source "$SCRIPT_DIR/base/timezone.sh"
